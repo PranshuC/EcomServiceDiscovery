8d7eccfdd7e175ff4e3916e902197fd8cc07e320
Pranshu Chakraborty <pranshu.c1@gmail.com>
2024-09-25 18:53:41 +0530

    Initial commit

---
+++ src/main/java/com/pranshu/ecomservicediscovery/EcomServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.pranshu.ecomservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomServiceDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomServiceDiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pranshu/ecomservicediscovery/EcomServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranshu.ecomservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
